cc9a03a6244479d3a24f439ec1203192821108d3
riyaz <riyaz.s@osplabs.com>
2024-12-08 20:23:23 +0530

    Added pics

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,22 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#010922",
+        secondary: "#8A89DA",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
       },
+      fontFamily: {
+        outfit: ["Outfit", "serif"],
+      },
+    },
+    screens: {
+      xs: "425px",
+      ss: "640px",
+      sm: "768px",
+      md: "1024px",
+      lg: "1280px",
+      xl: "1536px",
     },
   },
   plugins: [],
